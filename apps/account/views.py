423c092c656bc1cd91643cfaf61e5b954ac6f9a0
TimaDootaliev <timur.dootaliev.td@gmail.com>
2022-10-19 20:20:50 +0600

    celery

--- apps/account/views.py
+++ apps/account/views.py
@@ -22,6 +22,22 @@ class RegistrationView(APIView):
                 status=status.HTTP_201_CREATED
             )
 
-# TODO: Активация, смена пароля, удаление аккаунта, восстановления пароля
-# TODO: подключить celery, redis
-# TODO: исправить HTML
+class AccountActivationView(APIView):
+    def get(self, request, activation_code):
+        user = User.objects.filter(activation_code=activation_code).first()
+        if not user:
+            return Response(
+                'Page not found', 
+                status=status.HTTP_404_NOT_FOUND
+                )
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+        return Response(
+            'Account activated! You can login now', 
+            status=status.HTTP_200_OK
+            )
+
+
+
+# TODO: смена пароля, удаление аккаунта, восстановления пароля
